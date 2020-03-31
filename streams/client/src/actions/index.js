63897605235494b0c616309c8c293ac7eee9241c
ericsmith92 <ericsmth884@gmail.com>
2020-03-31 08:42:06 -0400

    switch to normal Router to maintain our own history object for use in the application

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -29,6 +29,8 @@ export const createStream = formValues => async (dispatch, getState) => {
     const response = await streams.post('/streams', { ...formValues, userId });
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
+    //do some programmatic navigation to get user back to root route
+    
 };
 
 export const fetchStreams = () => async dispatch => {
