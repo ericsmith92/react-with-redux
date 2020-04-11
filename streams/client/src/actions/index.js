e5ee155122e88fd5bb4c68def61d27eb720da419
ericsmith92 <ericsmth884@gmail.com>
2020-04-11 17:03:32 -0400

    delete stream complete

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -58,6 +58,7 @@ export const deleteStream = (id) => async dispatch => {
     await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
+    history.push('/');
 };
 
 
